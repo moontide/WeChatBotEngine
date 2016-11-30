344317fc821f3dbb821b4042df59792e47817535
LiuYan 刘研 <lovetide@qq.com>
2016-11-30 21:47:08 +0800

    复读机机器人复读消息时附加时间戳已取消，统一由 BotEngine 处理。配置项: engine.message.text.appendTimestamp=yes|no|y|n|是|否|true|false|t|f|1|0

--- bots/net_maclife_wechat_http_Bot_Repeater.java
+++ bots/net_maclife_wechat_http_Bot_Repeater.java
@@ -8,14 +8,17 @@ import com.fasterxml.jackson.core.*;
 
 public class net_maclife_wechat_http_Bot_Repeater extends net_maclife_wechat_http_Bot
 {
+	boolean bRepeatMyOwnMessage = net_maclife_wechat_http_BotApp.ParseBoolean (net_maclife_wechat_http_BotApp.config.getString ("bot.repeater.repeatMyOwnMessage", "no"), false);
+
 	@Override
 	public int OnTextMessageReceived (String sFrom_RoomAccountHash, String sFrom_RoomNickName, String sFrom_AccountHash, String sFrom_NickName, String sTo_AccountHash, String sTo_NickName, String sMessage)
 	{
-		//boolean isFromRoomOrChannel = isChatRoomAccount (sFrom_AccountHash);
-		//boolean isToRoomOrChannel = isChatRoomAccount (sTo_AccountHash);
+		if (!bRepeatMyOwnMessage && engine.IsMe (sFrom_AccountHash))
+			return net_maclife_wechat_http_BotApp.BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
+
 		try
 		{
-			engine.SendTextMessage (sFrom_RoomAccountHash, sFrom_AccountHash, sFrom_NickName, sMessage + "\n\n" + new java.sql.Timestamp(System.currentTimeMillis ()));
+			engine.SendTextMessage (sFrom_RoomAccountHash, sFrom_AccountHash, sFrom_NickName, sMessage);
 		}
 		catch (Exception e)
 		{
