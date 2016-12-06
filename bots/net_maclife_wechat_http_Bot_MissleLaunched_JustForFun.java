d31f3e7d4565fb85c4721d41489d10cb61b6fcc7
LiuYan 刘研 <lovetide@qq.com>
2016-12-07 00:30:35 +0800

    增加了“导弹已发射”的恶搞 Bot：当有人发位置信息时，则回复配置好的恶搞消息（配置多条，随机选一条）

---
+++ bots/net_maclife_wechat_http_Bot_MissleLaunched_JustForFun.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+import org.apache.commons.lang3.*;
+
+import com.fasterxml.jackson.databind.*;
+
+/**
+ * 当有人发位置消息时，回复一段“导弹已就位”/“导弹已发射”的搞笑文字。
+ * @author liuyan
+ *
+ */
+public class net_maclife_wechat_http_Bot_MissleLaunched_JustForFun extends net_maclife_wechat_http_Bot
+{
+	@Override
+	public int OnGeoLocationMessageReceived (String sFrom_EncryptedRoomAccount, String sFrom_RoomNickName, String sFrom_EncryptedAccount, String sFrom_NickName, String sTo_EncryptedAccount, String sTo_NickName, JsonNode jsonMessage, String sLocation, String sLongtitude, String sLatitude)
+	{
+		try
+		{
+			String sReply = null;
+			List<String> listReplies = net_maclife_wechat_http_BotApp.config.getList (String.class, "bot.misslelaunched.replies");
+			if (listReplies!=null && listReplies.size ()>0)
+			{
+				int iRandom = net_maclife_wechat_http_BotApp.random.nextInt (listReplies.size ());
+				sReply = listReplies.get (iRandom);
+			}
+
+			SendTextMessage (sFrom_EncryptedRoomAccount, sFrom_EncryptedAccount, sFrom_NickName, "经度: " + sLongtitude + "\n纬度: " + sLatitude + "\n位置: " + sLocation + (StringUtils.isEmpty (sReply) ? "" : "\n" + sReply));
+			return
+				net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__PROCESSED
+				| net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace ();
+		}
+		return net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
+	}
+}
