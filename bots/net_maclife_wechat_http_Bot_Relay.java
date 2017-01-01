f01ee312be77058da356935eb4c4e6d3822bf9b6
LiuYan 刘研 <lovetide@qq.com>
2017-01-02 03:04:20 +0800

    Relay 消息中继机器人小程序增加了通过 Account/ID 来指定接收人的功能，这个 ID ，通常，必须是明文 ID，类似于 wxid_XXXX (比较常见的个人帐号)、 gh_XXX (公众号帐号)、 NNNNNN@chatroom (群聊帐号)、还有一些用户自定义的帐号（早期注册的？）。 这个明文 ID，在手机端打开一个聊天窗口时，会有 MsgType=51 的消息过来，里面会看到这个聊天窗口对方的明文 ID。

--- bots/net_maclife_wechat_http_Bot_Relay.java
+++ bots/net_maclife_wechat_http_Bot_Relay.java
@@ -17,9 +17,17 @@ import com.fasterxml.jackson.databind.*;
 {
 	From: "",	// 来自，可以是任意内容，但通常指定应用程序名称及版本。该字段可省略（等同于值为空的 From）
 	To:	// 消息接收人，接收人可以是单个对象、也可以多个（数组）。因为微信不能用一个唯一 ID (加密且每次登录会改变) 来指定接收人，
-		// 所以需要通过指定 Alias、RemarkName、NickName、DisplayName 几个名称中的一个或多个的方式来定位该接收人。
+		// 所以需要通过指定 Account、Alias、RemarkName、NickName、DisplayName 几个名称中的一个或多个的方式来定位该接收人。
 		[
 			{	// 这几个字段，至少需要指定其中的一个。
+				Account: "明文帐号，或通配符帐号",	// 明文帐号： 是在手机端打开一个联系人聊天窗口，然后在网页端获取到的明文，诸如： wxid_XXXX (比较常见的个人帐号)、 gh_XXX (公众号帐号)、 NNNNNN@chatroom (群聊帐号)、还有一些用户自定义的帐号（早期注册的？）
+						// 通配符帐号： 通配符帐号的目的是为了群发用。目前支持的通配符帐号有：
+						// <code>*</code> - 通讯录里的所有联系人；含群聊天室、公众号（含类似“微信团队”之类的）；
+						// <code>*p</code> - 通讯录除去群聊天室、公众号后的联系人 (人/<font color='red'>P</font>erson，但含类似“文件传输助手”这类的非“人”的帐号 -- 暂时没法区分)；
+						// <code>*w</code> 或 <code>*g</code> - 所有联系人 (女人/<font color='red'>W</font>oman/<font color='red'>G</font>irl)；
+						// <code>*m</code> 或 <code>*b</code> - 所有联系人 (男人/<font color='red'>M</font>an/<font color='red'>B</font>oy)；
+						// <code>*r</code> - 通讯录里的所有聊天室联系人 (<font color='red'>R</font>oom)；
+						// <code>*gh</code> - 通讯录里的所有公众号联系人；(<font color='red'>gh</font> 是公众号的明文 ID/明文 Account 的帐号前缀，g公 h号？)
 				Alias: "别名",
 				RemarkName: "备注名",
 				NickName: "昵称",	// 昵称可能会被好友自己改动，所以，不建议用这个
@@ -127,27 +135,13 @@ net_maclife_wechat_http_BotApp.logger.severe (GetName () + " 启动失败: " + e
 				for (int i=0; i<jsonTOs.size (); i++)
 				{
 					JsonNode jsonTo = jsonTOs.get (i);
-					String sAliasAccount = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "Alias");
-					String sRemarkName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "RemarkName");
-					String sNickName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "NickName");
-					//String sDisplayName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "DisplayName");
-
-					JsonNode jsonContact = engine.SearchForSingleContact (null, sAliasAccount, sRemarkName, sNickName);
-					if (jsonContact != null)
-						listTOs.add (net_maclife_wechat_http_BotApp.GetJSONText (jsonContact, "UserName"));
+					BuildRecipients (jsonTo, listTOs);
 				}
 			}
 			else
 			{
 				JsonNode jsonTo = jsonTOs;
-				String sAliasAccount = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "Alias");
-				String sRemarkName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "RemarkName");
-				String sNickName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "NickName");
-				//String sDisplayName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "DisplayName");
-
-				JsonNode jsonContact = engine.SearchForSingleContact (null, sAliasAccount, sRemarkName, sNickName);
-				if (jsonContact != null)
-					listTOs.add (net_maclife_wechat_http_BotApp.GetJSONText (jsonContact, "UserName"));
+				BuildRecipients (jsonTo, listTOs);
 			}
 
 			if (listTOs.isEmpty ())
@@ -181,4 +175,106 @@ net_maclife_wechat_http_BotApp.logger.severe (GetName () + " 启动失败: " + e
 		}
 	}
 
+	void BuildRecipients (JsonNode jsonTo, List<String> listTOs)
+	{
+			String sAccount = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "Account");
+			String sAlias = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "Alias");
+			String sRemarkName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "RemarkName");
+			String sNickName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "NickName");
+			//String sDisplayName = net_maclife_wechat_http_BotApp.GetJSONText (jsonTo, "DisplayName");
+
+			if (StringUtils.isEmpty (sAccount))
+			{
+				JsonNode jsonContact = engine.SearchForSingleContact (null, sAlias, sRemarkName, sNickName);
+				if (jsonContact != null)
+					listTOs.add (net_maclife_wechat_http_BotApp.GetJSONText (jsonContact, "UserName"));
+			}
+			else
+			{
+				if (StringUtils.equalsAnyIgnoreCase (sAccount, "*"))	// 所有联系人
+				{
+					if (engine.jsonContacts == null)
+						return;
+
+					JsonNode jsonContactList = engine.jsonContacts.get ("MemberList");
+					for (JsonNode jsonContact : jsonContactList)
+						listTOs.add (net_maclife_wechat_http_BotApp.GetJSONText (jsonContact, "UserName"));
+				}
+				else if (StringUtils.equalsAnyIgnoreCase (sAccount, "*p"))	// 所有“人”（排除群聊、公众号），但其实，也可能包含类似 filehelper 之类的
+				{
+					if (engine.jsonContacts == null)
+						return;
+
+					JsonNode jsonContactList = engine.jsonContacts.get ("MemberList");
+					for (JsonNode jsonContact : jsonContactList)
+					{
+						String sEncryptedAccount = net_maclife_wechat_http_BotApp.GetJSONText (jsonContact, "UserName");
+						int nVerifyFlag = net_maclife_wechat_http_BotApp.GetJSONInt (jsonContact, "VerifyFlag");
+						boolean isPublicAccount = net_maclife_wechat_http_BotApp.IsPublicAccount (nVerifyFlag);
+						boolean isRoomAccount = net_maclife_wechat_http_BotApp.IsRoomAccount (sEncryptedAccount);
+						if (isPublicAccount || isRoomAccount)
+							continue;
+
+						listTOs.add (sEncryptedAccount);
+					}
+				}
+				else if (StringUtils.equalsAnyIgnoreCase (sAccount, "*m") || StringUtils.equalsAnyIgnoreCase (sAccount, "*b")	// 所有“男人/Man/Boy”（排除群聊、公众号）
+						|| StringUtils.equalsAnyIgnoreCase (sAccount, "*w") || StringUtils.equalsAnyIgnoreCase (sAccount, "*g"))	// 所有“女人/Woman/Girl”（排除群聊、公众号）
+				{
+					if (engine.jsonContacts == null)
+						return;
+
+					JsonNode jsonContactList = engine.jsonContacts.get ("MemberList");
+					for (JsonNode jsonContact : jsonContactList)
+					{
+						String sEncryptedAccount = net_maclife_wechat_http_BotApp.GetJSONText (jsonContact, "UserName");
+						int nVerifyFlag = net_maclife_wechat_http_BotApp.GetJSONInt (jsonContact, "VerifyFlag");
+						boolean isPublicAccount = net_maclife_wechat_http_BotApp.IsPublicAccount (nVerifyFlag);
+						boolean isRoomAccount = net_maclife_wechat_http_BotApp.IsRoomAccount (sEncryptedAccount);
+						if (isPublicAccount || isRoomAccount)
+							continue;
+
+						int nGender = net_maclife_wechat_http_BotApp.GetJSONInt (jsonContact, "Sex");
+						if (nGender == 1 && (StringUtils.equalsAnyIgnoreCase (sAccount, "*m") || StringUtils.equalsAnyIgnoreCase (sAccount, "*b"))
+							||
+							nGender == 2 && (StringUtils.equalsAnyIgnoreCase (sAccount, "*w") || StringUtils.equalsAnyIgnoreCase (sAccount, "*g"))
+							)
+							listTOs.add (sEncryptedAccount);
+					}
+				}
+				else if (StringUtils.equalsAnyIgnoreCase (sAccount, "*r"))	// 所有群聊
+				{
+					if (engine.jsonContacts == null)
+						return;
+
+					JsonNode jsonContactList = engine.jsonContacts.get ("MemberList");
+					for (JsonNode jsonContact : jsonContactList)
+					{
+						String sEncryptedAccount = net_maclife_wechat_http_BotApp.GetJSONText (jsonContact, "UserName");
+						boolean isRoomAccount = net_maclife_wechat_http_BotApp.IsRoomAccount (sEncryptedAccount);
+						if (isRoomAccount)
+							listTOs.add (sEncryptedAccount);
+					}
+				}
+				else if (StringUtils.equalsAnyIgnoreCase (sAccount, "*gh"))	// 所有公众号，也不知道有没有人会专门发到所有公众号里…
+				{
+					if (engine.jsonContacts == null)
+						return;
+
+					JsonNode jsonContactList = engine.jsonContacts.get ("MemberList");
+					for (JsonNode jsonContact : jsonContactList)
+					{
+						String sEncryptedAccount = net_maclife_wechat_http_BotApp.GetJSONText (jsonContact, "UserName");
+						int nVerifyFlag = net_maclife_wechat_http_BotApp.GetJSONInt (jsonContact, "VerifyFlag");
+						boolean isPublicAccount = net_maclife_wechat_http_BotApp.IsPublicAccount (nVerifyFlag);
+						if (isPublicAccount)
+							listTOs.add (sEncryptedAccount);
+					}
+				}
+				else
+				{	// 非特殊形式的帐号字符串，则直接当成帐号
+					listTOs.add (sAccount);
+				}
+			}
+	}
 }
