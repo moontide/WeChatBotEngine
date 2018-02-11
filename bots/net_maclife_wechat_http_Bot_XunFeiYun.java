af05e80a9cda8d5baf43f34fec1add835db7a731
LiuYan 刘研 <lovetide@qq.com>
2018-02-11 18:30:22 +0800

    【讯飞云】增加了语义理解功能，含文本语义理解、语音语义理解。其中，文本语义理解的使用方法：必须在文本消息最开头 @ 本机器人

--- bots/net_maclife_wechat_http_Bot_XunFeiYun.java
+++ bots/net_maclife_wechat_http_Bot_XunFeiYun.java
@@ -8,39 +8,54 @@ import org.apache.commons.lang3.*;
 
 //import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.node.*;
 
 /**
- * 利用讯飞云的语音听写（免费） API 实现的 语音识别（语音转文字） 功能机器人。
+ * 利用讯飞云的<a href='http://aiui.xfyun.cn/help/devDoc#3-3-3'>语音听写（免费）</a>、<a href='http://aiui.xfyun.cn/help/devDoc#3-3-2'>文本语义理解</a>、<a href='http://aiui.xfyun.cn/help/devDoc#3-3-4'>语音语义理解</a> 等 API 实现的 语音识别（语音转文字）、语义理解（理解用户意图并给出回复/答案）功能机器人。
 <p>
-限制：“语音听写”只提供对语音时长在一分钟之内的语音进行识别。对时长更长的语音的识别需要用另外一个 API （收费）。
+限制：“语音听写”功能只提供对语音时长在一分钟之内的语音进行识别。对时长更长的语音的识别需要用另外一个 API （收费）。
 </p>
-<p>
-	百度访问令牌 (AccessToken) 返回的数据格式
-<pre>
-{
-    "access_token":"***...***",
-    "session_key":"***...***",
-    "scope":"public audio_voice_assistant_get audio_tts_post wise_adapt lebo_resource_base lightservice_public hetu_basic lightcms_map_poi kaidian_kaidian",	// 类似这样
-    "refresh_token":"***...***",
-    "session_secret":"***...***",
-    "expires_in":NNNNNNN
-}
-</pre>
 
-语音识别结果
+<p>
+讯飞云语音识别结果示例：
 <pre>
 {
-    "corpus_no":"******",
-    "err_msg":"*****.",
-    "err_no":0,	// 0 是成功，其他为失败
-    "result":	// 最多 5 个结果，或者根本不存在（出错时）
-    [
-        "账号已暂停使用，"
-    ],
-    "sn":"*****"
+    "code": "00000",
+    "desc": "成功",
+    "data": {
+        "ret": 0,
+        "result": "今天星期几。",
+        "sid": "watb37fe700@ch47730ce51e04477300"
+    },
+    "sid":"rwa8066ef80@cha4320da12234000100"
 }
 </pre>
 </p>
+
+讯飞云 语义理解（文本、语音的语义理解结果格式相同） 结果示例：
+<pre>
+{
+  "code": "00000",
+  "desc": "成功",
+  "data": {
+    "answer": {
+      "text": "今天是2017年08月08日 丁酉年六月十七 星期二",
+      "type": "T"
+    },
+    "match_info": {
+      "type": "gparser_path",
+      "value": "-----"
+    },
+    "operation": "ANSWER",
+    "rc": 0,
+    "service": "datetime",
+    "text": "今天星期几",
+    "uuid": "atn00210ce6@un782b0ce4cac76f2601",
+    "sid": "rwa2ac04d1c@chfca30da12150000100"
+  },
+  "sid":"rwa2ac04d1c@chfca30da12150000100"
+}
+</pre>
 
  * @author liuyan
  *
@@ -48,9 +63,35 @@ import com.fasterxml.jackson.databind.*;
 public class net_maclife_wechat_http_Bot_XunFeiYun extends net_maclife_wechat_http_Bot
 {
 	public static final String XFYUN_ASR_API_URL = "http://api.xfyun.cn/v1/aiui/v1/iat";
+	public static final String XFYUN_TEXT_SEMANTIC_API_URL = "http://api.xfyun.cn/v1/aiui/v1/text_semantic";
+	public static final String XFYUN_VOICE_SEMANTIC_API_URL = "http://api.xfyun.cn/v1/aiui/v1/voice_semantic";
 
 	static String sXunFeiYunAppID = net_maclife_wechat_http_BotApp.GetConfig ().getString ("bot.xfyun.app.id");
 	static String sXunFeiYunAppKey = net_maclife_wechat_http_BotApp.GetConfig ().getString ("bot.xfyun.app.key");
+
+	@Override
+	public int OnTextMessageReceived
+		(
+			JsonNode jsonMessage,
+			JsonNode jsonFrom, String sFromAccount, String sFromName, boolean isFromMe,
+			JsonNode jsonTo, String sToAccount, String sToName, boolean isToMe,
+			JsonNode jsonReplyTo, String sReplyToAccount, String sReplyToName, boolean isReplyToRoom,
+			JsonNode jsonReplyTo_RoomMember, String sReplyToAccount_RoomMember, String sReplyToName_RoomMember,
+			JsonNode jsonReplyTo_Person, String sReplyToAccount_Person, String sReplyToName_Person,
+			String sContent, boolean isContentMentionedMe, boolean isContentMentionedMeFirst
+		)
+	{
+		try
+		{
+			if (net_maclife_wechat_http_BotApp.ParseBoolean (net_maclife_wechat_http_BotApp.GetConfig ().getString ("bot.xfyun.text-semantic.enabled")) && (!isReplyToRoom || isContentMentionedMeFirst))
+				return ProcessTextSemantic (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, sReplyToAccount_Person, sReplyToName_Person, sContent);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
+	}
 
 	@Override
 	public int OnVoiceMessageReceived
@@ -76,8 +117,10 @@ public class net_maclife_wechat_http_Bot_XunFeiYun extends net_maclife_wechat_ht
 			{
 				return net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
 			}
-			ProcessSpeechRecognition (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, fMedia);
-
+			if (net_maclife_wechat_http_BotApp.ParseBoolean (net_maclife_wechat_http_BotApp.GetConfig ().getString ("bot.xfyun.voice-semantic.enabled")))
+				return ProcessVoiceSemantic (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, sReplyToAccount_Person, sReplyToName_Person, fMedia);
+			else
+				return ProcessSpeechRecognition (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, fMedia);
 		}
 		catch (Exception e)
 		{
@@ -108,8 +151,10 @@ public class net_maclife_wechat_http_Bot_XunFeiYun extends net_maclife_wechat_ht
 			{
 				return net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
 			}
-			ProcessSpeechRecognition (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, fMedia);
-
+			if (net_maclife_wechat_http_BotApp.ParseBoolean (net_maclife_wechat_http_BotApp.GetConfig ().getString ("bot.xfyun.voice-semantic.enabled")))
+				return ProcessVoiceSemantic (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, sReplyToAccount_Person, sReplyToName_Person, fMedia);
+			else
+				return ProcessSpeechRecognition (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, fMedia);
 		}
 		catch (Exception e)
 		{
@@ -159,16 +204,17 @@ curl -v api.xfyun.cn/v1/aiui/v1/iat -H "X-Appid: $appid " -H "X-CurTime: $t" -H 
 		mapRequestHeaders.put ("X-Param", sXfyunParam_Base64);
 		String sValidationHashString = DigestUtils.md5Hex (sXunFeiYunAppKey + sCurrentUnixTimestamp + sXfyunParam_Base64 + sHTTPBody_Base64);
 		mapRequestHeaders.put ("X-CheckSum", sValidationHashString);
+net_maclife_wechat_http_BotApp.logger.finest ("讯飞云 语音听写/语音识别");
 net_maclife_wechat_http_BotApp.logger.finest ("AppID: " + sXunFeiYunAppID);
 net_maclife_wechat_http_BotApp.logger.finest ("AppKey: " + sXunFeiYunAppKey);
 net_maclife_wechat_http_BotApp.logger.finest ("Tstmp: " + sCurrentUnixTimestamp);
 net_maclife_wechat_http_BotApp.logger.finest ("Param: " + sXfyunParam_Base64);
-net_maclife_wechat_http_BotApp.logger.finest ("HTBdy: " + sHTTPBody_Base64);
+//net_maclife_wechat_http_BotApp.logger.finest ("HTBdy: " + sHTTPBody_Base64);
 net_maclife_wechat_http_BotApp.logger.finest ("Hash : " + sValidationHashString);
 
 		String sResponseBodyContent = net_maclife_util_HTTPUtils.CURL_Post (XFYUN_ASR_API_URL, mapRequestHeaders, sHTTPBody_Base64.getBytes ());
 		JsonNode jsonResult = net_maclife_wechat_http_BotApp.jacksonObjectMapper_Loose.readTree (sResponseBodyContent);
-net_maclife_wechat_http_BotApp.logger.info (GetName() + " 机器人获取讯飞云语音识别 (ASR) 的 http 响应消息体:");
+net_maclife_wechat_http_BotApp.logger.info (GetName() + " 机器人获取讯飞云【语音识别 (ASR)】的 http 响应消息体:");
 net_maclife_wechat_http_BotApp.logger.info ("	" + sResponseBodyContent);
 
 		String  sRC = net_maclife_wechat_http_BotApp.GetJSONText (jsonResult, "code");
@@ -181,6 +227,8 @@ net_maclife_wechat_http_BotApp.logger.info ("	" + sResponseBodyContent);
 		{
 			case 0:
 				String sResult = net_maclife_wechat_http_BotApp.GetJSONText (jsonData, "result");
+				if (StringUtils.isEmpty(sResult))
+					return net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
 				SendTextMessage (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, (StringUtils.isEmpty (sReplyToAccount_RoomMember) ? sReplyToName : sReplyToName_RoomMember) + " 说道:\n" + sResult);
 					return
 						net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__PROCESSED
@@ -257,6 +305,171 @@ net_maclife_wechat_http_BotApp.logger.severe ("视频提取音频失败");
 		return new File (sWavFileName);
 	}
 
+	int ProcessTextSemantic
+		(
+			String sReplyToAccount, String sReplyToName,
+			String sReplyToAccount_RoomMember, String sReplyToName_RoomMember,
+			String sReplyToAccount_Person, String sReplyToName_Person,
+			String sContent
+		) throws Exception
+	{
+		String sFileData_Base64 = Base64.encodeBase64String (sContent.getBytes (net_maclife_wechat_http_BotApp.utf8));
+		//String sFileData_Base64_URLEncoded = URLEncoder.encode (sFileData_Base64, net_maclife_wechat_http_BotApp.utf8);
+		String sHTTPBody_Base64 = "text=" + sFileData_Base64;
+		//String sHTTPBody_Base64_URLEncoded = "data=" + sFileData_Base64_URLEncoded;
+
+		Map<String, Object> mapRequestHeaders = new HashMap<String, Object> ();
+		mapRequestHeaders.put ("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+		mapRequestHeaders.put ("X-Appid", sXunFeiYunAppID);
+		String sCurrentUnixTimestamp = String.valueOf (System.currentTimeMillis () / 1000);
+		mapRequestHeaders.put ("X-CurTime", sCurrentUnixTimestamp);
+		String sXfyunParam_Base64 = Base64.encodeBase64String (("{\"scene\":\"main\",\"userid\":\"" + StringUtils.strip (sReplyToAccount_Person, "@") + "\"}").getBytes (net_maclife_wechat_http_BotApp.utf8));
+		mapRequestHeaders.put ("X-Param", sXfyunParam_Base64);
+		String sValidationHashString = DigestUtils.md5Hex (sXunFeiYunAppKey + sCurrentUnixTimestamp + sXfyunParam_Base64 + sHTTPBody_Base64);
+		mapRequestHeaders.put ("X-CheckSum", sValidationHashString);
+net_maclife_wechat_http_BotApp.logger.finest ("讯飞云 文本语义理解");
+net_maclife_wechat_http_BotApp.logger.finest ("AppID: " + sXunFeiYunAppID);
+net_maclife_wechat_http_BotApp.logger.finest ("AppKey: " + sXunFeiYunAppKey);
+net_maclife_wechat_http_BotApp.logger.finest ("Tstmp: " + sCurrentUnixTimestamp);
+net_maclife_wechat_http_BotApp.logger.finest ("Param: " + sXfyunParam_Base64);
+net_maclife_wechat_http_BotApp.logger.finest ("HTBdy: " + sHTTPBody_Base64);
+net_maclife_wechat_http_BotApp.logger.finest ("Hash : " + sValidationHashString);
+
+		String sResponseBodyContent = net_maclife_util_HTTPUtils.CURL_Post (XFYUN_TEXT_SEMANTIC_API_URL, mapRequestHeaders, sHTTPBody_Base64.getBytes ());
+		JsonNode jsonResult = net_maclife_wechat_http_BotApp.jacksonObjectMapper_Loose.readTree (sResponseBodyContent);
+net_maclife_wechat_http_BotApp.logger.info (GetName() + " 机器人获取讯飞云【文本语义理解】的 http 响应消息体:");
+net_maclife_wechat_http_BotApp.logger.info ("	" + sResponseBodyContent);
+
+		return ProcessSemanticResult (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, sReplyToAccount_Person, sReplyToName_Person, jsonResult);
+	}
+
+	int ProcessVoiceSemantic
+		(
+			String sReplyToAccount, String sReplyToName,
+			String sReplyToAccount_RoomMember, String sReplyToName_RoomMember,
+			String sReplyToAccount_Person, String sReplyToName_Person,
+			File fMedia
+		) throws Exception
+	{
+		InputStream is = new FileInputStream (fMedia);
+		byte[] arrayFileData = IOUtils.toByteArray (is, fMedia.length ());
+		is.close ();
+		String sFileData_Base64 = Base64.encodeBase64String (arrayFileData);
+		//String sFileData_Base64_URLEncoded = URLEncoder.encode (sFileData_Base64, net_maclife_wechat_http_BotApp.utf8);
+		String sHTTPBody_Base64 = "text=" + sFileData_Base64;
+		//String sHTTPBody_Base64_URLEncoded = "data=" + sFileData_Base64_URLEncoded;
+
+		Map<String, Object> mapRequestHeaders = new HashMap<String, Object> ();
+		mapRequestHeaders.put ("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+		mapRequestHeaders.put ("X-Appid", sXunFeiYunAppID);
+		String sCurrentUnixTimestamp = String.valueOf (System.currentTimeMillis () / 1000);
+		mapRequestHeaders.put ("X-CurTime", sCurrentUnixTimestamp);
+		String sXfyunParam_Base64 = Base64.encodeBase64String (("{\"auf\":\"8k\",\"aue\":\"raw\",\"scene\":\"main\",\"userid\":\"" + StringUtils.strip (sReplyToAccount_Person, "@") + "\"}").getBytes (net_maclife_wechat_http_BotApp.utf8));
+		mapRequestHeaders.put ("X-Param", sXfyunParam_Base64);
+		String sValidationHashString = DigestUtils.md5Hex (sXunFeiYunAppKey + sCurrentUnixTimestamp + sXfyunParam_Base64 + sHTTPBody_Base64);
+		mapRequestHeaders.put ("X-CheckSum", sValidationHashString);
+net_maclife_wechat_http_BotApp.logger.finest ("讯飞云 语音语义理解");
+net_maclife_wechat_http_BotApp.logger.finest ("AppID: " + sXunFeiYunAppID);
+net_maclife_wechat_http_BotApp.logger.finest ("AppKey: " + sXunFeiYunAppKey);
+net_maclife_wechat_http_BotApp.logger.finest ("Tstmp: " + sCurrentUnixTimestamp);
+net_maclife_wechat_http_BotApp.logger.finest ("Param: " + sXfyunParam_Base64);
+//net_maclife_wechat_http_BotApp.logger.finest ("HTBdy: " + sHTTPBody_Base64);
+net_maclife_wechat_http_BotApp.logger.finest ("Hash : " + sValidationHashString);
+
+		String sResponseBodyContent = net_maclife_util_HTTPUtils.CURL_Post (XFYUN_VOICE_SEMANTIC_API_URL, mapRequestHeaders, sHTTPBody_Base64.getBytes ());
+		JsonNode jsonResult = net_maclife_wechat_http_BotApp.jacksonObjectMapper_Loose.readTree (sResponseBodyContent);
+net_maclife_wechat_http_BotApp.logger.info (GetName() + " 机器人获取讯飞云【语音语义理解】的 http 响应消息体:");
+net_maclife_wechat_http_BotApp.logger.info ("	" + sResponseBodyContent);
+
+		return ProcessSemanticResult (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, sReplyToAccount_Person, sReplyToName_Person, jsonResult);
+	}
+	int ProcessSemanticResult
+		(
+			String sReplyToAccount, String sReplyToName,
+			String sReplyToAccount_RoomMember, String sReplyToName_RoomMember,
+			String sReplyToAccount_Person, String sReplyToName_Person,
+			JsonNode jsonSemanticResult
+		) throws Exception
+	{
+		String  sRC = net_maclife_wechat_http_BotApp.GetJSONText (jsonSemanticResult, "code");
+		//String sSID = net_maclife_wechat_http_BotApp.GetJSONText (jsonResult, "sid");
+		String sDesc = net_maclife_wechat_http_BotApp.GetJSONText (jsonSemanticResult, "desc");
+		JsonNode jsonData = jsonSemanticResult.get ("data");
+		String sTextInputed = net_maclife_wechat_http_BotApp.GetJSONText (jsonData, "text");
+		String sVendor = net_maclife_wechat_http_BotApp.GetJSONText (jsonData, "vendor");
+		String sService = net_maclife_wechat_http_BotApp.GetJSONText (jsonData, "service");
+		ArrayNode jsonSemantics = (ArrayNode)jsonData.get ("semantic");
+		JsonNode jsonFirstSemantic = null;
+		if (jsonSemantics != null)
+			jsonFirstSemantic = jsonSemantics.get (0);
+		String sFirstSementic_Intent = "";
+		ArrayNode jsonFirstSemantic_Slots = null;
+		if (jsonFirstSemantic != null)
+		{
+			sFirstSementic_Intent = net_maclife_wechat_http_BotApp.GetJSONText (jsonFirstSemantic, "intent");
+			jsonFirstSemantic_Slots = (ArrayNode)jsonFirstSemantic.get ("slots");
+		}
+		JsonNode jsonDataData = jsonData.get ("data");
+		JsonNode jsonDataDataResult = null;
+		JsonNode jsonFirstDataDataResult = null;
+		if (jsonDataData!=null)
+		{
+			jsonDataDataResult = jsonDataData.get ("result");
+			if (jsonDataDataResult != null && ((ArrayNode)jsonDataDataResult).size () > 0)
+				jsonFirstDataDataResult = ((ArrayNode)jsonDataDataResult).get (0);
+		}
+		JsonNode jsonAnswer = jsonData.get ("answer");
+		String sAnswer = net_maclife_wechat_http_BotApp.GetJSONText (jsonAnswer, "text");
+		String sDialogStat = net_maclife_wechat_http_BotApp.GetJSONText (jsonData, "dialog_stat");
+		JsonNode jsonMoreResults = jsonData.get ("moreResults");
+		String sDataSID = net_maclife_wechat_http_BotApp.GetJSONText (jsonData, "sid");
+		int nReturnCode = net_maclife_wechat_http_BotApp.GetJSONInt (jsonData, "rc");
+		//if (StringUtils.equalsIgnoreCase (sRC, "00000"))
+		//{
+		switch (nReturnCode)
+		{
+			case 0:
+				StringBuilder sbResult = new StringBuilder ();
+				sbResult.append ("问: " + sTextInputed + "\n");
+				if (StringUtils.isNotEmpty (sAnswer))
+					sbResult.append ("答: " + sAnswer + "\n");
+				if (StringUtils.isNotEmpty (sService))
+					sbResult.append ("服务: " + sService + "\n");
+				if (StringUtils.isNotEmpty (sVendor))
+					sbResult.append ("服务商: " + sVendor + "\n");
+				if (jsonFirstSemantic != null)
+				{
+					sbResult.append ("语义意图: " + sFirstSementic_Intent + "\n");
+					if (jsonFirstSemantic_Slots != null && jsonFirstSemantic_Slots.size ()>0)
+					{
+						sbResult.append ("语义分解:\n");
+						for (int i=0; i<jsonFirstSemantic_Slots.size (); i++)
+						{
+							JsonNode jsonTempNode = jsonFirstSemantic_Slots.get (i);
+							sbResult.append ("    " + net_maclife_wechat_http_BotApp.GetJSONText (jsonTempNode, "name") + ": " + net_maclife_wechat_http_BotApp.GetJSONText (jsonTempNode, "value") + "\n");
+						}
+					}
+				}
+				if (jsonFirstDataDataResult!=null)
+				{
+					sbResult.append ("数据条数: " + ((ArrayNode)jsonDataDataResult).size () + "\n");
+					sbResult.append ("首条数据: " + jsonFirstDataDataResult + "\n");
+				}
+				String sResult = sbResult.toString ();
+net_maclife_wechat_http_BotApp.logger.info (sResult);
+				if (StringUtils.isEmpty(sResult))
+					return net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
+				SendTextMessage (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, sResult);
+					return
+						net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__PROCESSED
+						| net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
+			default:
+net_maclife_wechat_http_BotApp.logger.warning (GetName() + " " + sRC + " " + sDesc + " data.rc=" + nReturnCode);
+				//SendTextMessage (sReplyToAccount, sReplyToName, sReplyToAccount_RoomMember, sReplyToName_RoomMember, "听不清 " + (StringUtils.isEmpty (sReplyToAccount_RoomMember) ? sReplyToName : sReplyToName_RoomMember) + " 说了些啥: " + err_msg);
+				break;
+		}
+		return net_maclife_wechat_http_BotEngine.BOT_CHAIN_PROCESS_MODE_MASK__CONTINUE;
+	}
 	public static void main (String[] args)
 	{
 		if (args.length < 1)
@@ -269,7 +482,8 @@ net_maclife_wechat_http_BotApp.logger.severe ("视频提取音频失败");
 		net_maclife_wechat_http_Bot_XunFeiYun bot = new net_maclife_wechat_http_Bot_XunFeiYun ();
 		try
 		{
-			bot.ProcessSpeechRecognition (null, null, null, null, fMedia);
+			//bot.ProcessSpeechRecognition (null, null, null, null, fMedia);
+			bot.ProcessTextSemantic (null, null, null, null, null, null, args[0]);
 		}
 		catch (Exception e)
 		{
